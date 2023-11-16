d9e075ce201968351a5d43dd475d7430921ce152
Shielamaye <143503450+Shielamaye@users.noreply.github.com>
2023-11-16 18:57:40 +0800

    Add files via upload

---
+++ 2 projects.java
@@ -0,0 +1,9 @@
+// src/com/example/package1/MainClass.java
+
+package com.example.package1;
+
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("Hello from Package 1!");
+    }
+}
---
+++ main2.java
@@ -0,0 +1,9 @@
+// src/com/example/package2/AnotherClass.java
+
+package com.example.package2;
+
+public class AnotherClass {
+    public void displayMessage() {
+        System.out.println("Hello from Package 2!");
+    }
+}
